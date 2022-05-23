8d3e4158f967f6d706ef451cb7dc5d7d9c7c5c2b
adamwilkinson3 <82132359+adamwilkinson3@users.noreply.github.com>
2022-05-23 02:22:25 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import Background from './Background'
+import proj1 from './assets/proj1.JPG'
+import proj2 from './assets/proj2.JPG'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 function App() {
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", scrollToTop);
+    return () => {
+      window.removeEventListener("beforeunload", scrollToTop);
+    };
+  }, []);
+  const scrollToTop = (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  };
+
+  const [scroll, setScroll] = useState(0);
+  const [content1, setContent1] = useState(true);
+  const [content2, setContent2] = useState(false);
+  const [content3, setContent3] = useState(false);
+  const [content4, setContent4] = useState(false);
+  const divider = 1/4;
+
+  window.addEventListener('scroll', () => {
+    let scr = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+    setScroll(scr);
+
+    if (scroll <= divider) {
+      setContent1(true)
+    } else {
+      setContent1(false)
+    }
+    if (scroll >= divider && scroll <= divider * 2) {
+      setContent2(true)
+    } else {
+      setContent2(false)
+    }
+    if (scroll >= divider * 2 && scroll <= divider * 3) {
+      setContent3(true)
+    } else {
+      setContent3(false)
+    }
+    if (scroll >= divider * 3) {
+      setContent4(true)
+    } else {
+      setContent4(false)
+    }
+
+  }, false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <Background />
+      <div className='snap'>
+        <div className={`${content1 ? 'header-in' : 'hidden'}`}>
+          <h1>My Portfolio</h1>
+        </div>
+      </div>
+      <div className='snap'>
+        <div className={`${content2 ? 'header-in' : 'hidden'}`}>
+          <div className='projdisplay'>
+            <a href="https://github.com/adamwilkinson3/avionics" target="_blank" rel="noreferrer">
+              <img src={proj1} alt='proj1' />
+            </a>
+          <div className='proj-footer'>
+              <p></p>
+              <h2>An avionics training app built with HTML, CSS, and JavaScript</h2>
+              <a href="https://github.com/adamwilkinson3/avionics" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='snap'>
+        <div className={`${content3 ? 'header-in' : 'hidden'}`}>
+        <div className='projdisplay'>
+            <a href="https://github.com/adamwilkinson3/tobispizza" target="_blank" rel="noreferrer">
+              <img src={proj2} alt='proj2' />
+            </a>
+            <div className='proj-footer'>
+              <p></p>
+              <h2>A pizza ordering website built with React and Redux</h2>
+              <a href="https://github.com/adamwilkinson3/tobispizza" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='snap'>
+        <div className={`${content4 ? 'header-in' : 'hidden'}`}>
+          <div class='contact'>
+            <a href="https://www.linkedin.com/in/adam-wilkinson-784793105/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+            <a href="https://github.com/adamwilkinson3?tab=repositories" target="_blank" rel="noreferrer"><FaGithub /></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Background.js
@@ -0,0 +1,77 @@
+import { useEffect } from 'react'
+import './background.css'
+
+function Background() {
+   
+    const a = 40;
+    const b = 22;
+    let x1;
+    let y1;
+    let x2;
+    let y2;
+    let rad = 0;
+    let rot = 2;
+    let r;
+    let speed;
+    let scrspd = 1;
+
+    const a2 = 50;
+    const b2 = 25;
+    const rot2 = .5;
+
+    const a3 = 20;
+    const b3 = 15;
+    const rot3 = 2.5;
+
+    useEffect (() => {
+        window.addEventListener('scroll', () => {
+            scrspd = 15;
+            setTimeout(() => {
+                scrspd = 1;
+            }, 500);
+        }, false)
+        setInterval(() => {
+            r = (a * b)/((b**2 - a**2) * (Math.cos(rad))**2 + a**2)**.5;
+            x1 = r * Math.cos(rad);
+            y1 = r * Math.sin(rad);
+            x2 = x1 * Math.cos(rot) - y1 * Math.sin(rot);
+            y2 = y1 * Math.cos(rot) + x1 * Math.sin(rot);
+            speed = (a-b) * Math.abs(Math.cos(rad)) + b;
+            rad = rad + speed*scrspd/36000;
+            document.body.style.setProperty('--x', x2 + 'vh');
+            document.body.style.setProperty('--y', y2 + 'vh');
+        }, 10)
+        setInterval(() => {
+            r = (a2 * b2)/((b2**2 - a2**2) * (Math.cos(rad))**2 + a2**2)**.5;
+            x1 = r * Math.cos(rad);
+            y1 = r * Math.sin(rad);
+            x2 = x1 * Math.cos(rot2) - y1 * Math.sin(rot2);
+            y2 = y1 * Math.cos(rot2) + x1 * Math.sin(rot2);
+            speed = (a2-b2) * Math.abs(Math.cos(rad)) + b2;
+            rad = rad + speed*scrspd/36000;
+            document.body.style.setProperty('--x2', x2 + 'vh');
+            document.body.style.setProperty('--y2', y2 + 'vh');
+        }, 15)
+        setInterval(() => {
+            r = (a3 * b3)/((b3**2 - a3**2) * (Math.cos(-rad))**2 + a3**2)**.5;
+            x1 = r * Math.cos(-rad);
+            y1 = r * Math.sin(-rad);
+            x2 = x1 * Math.cos(rot3) - y1 * Math.sin(rot3);
+            y2 = y1 * Math.cos(rot3) + x1 * Math.sin(rot3);
+            speed = (a3-b3) * Math.abs(Math.cos(-rad)) + b3;
+            rad = rad + speed*scrspd/36000;
+            document.body.style.setProperty('--x3', x2 + 'vh');
+            document.body.style.setProperty('--y3', y2 + 'vh');
+        }, 15)
+    }, [])
+    
+  return (
+    <>
+        <div className='dot dota'></div>
+        <div className='dot dotb'></div>
+        <div className='dot dotc'></div>
+    </>
+  )
+}
+
+export default Background
